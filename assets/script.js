f324ec5616cdece8d4e3fd7d10d51a7b92147823
Kevin Atwood <atwoodkevin22@gmail.com>
2023-06-29 22:09:28 -0500

    Assigned variables, wrote funtions for user to choose if they want lowercase letters, wrote functions generating random numbers, started function to iterate over an array of arrays

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,68 @@
-// Assignment code here
-var alphabet = 'abcdefghijklmnopqrstuvwxyz'
-var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-var numbers  = '1234567890'
+// Assign Variables
+var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var numbers  = '1234567890';
 var lowerCase = alphabet.split('');
-var upperCase = alphabet.toUpperCase().split('')
-var specialChar = symbols.split('')
-var numChar = numbers.split('')
+var upperCase = alphabet.toUpperCase().split('');
+var specialChar = symbols.split('');
+var numChar = numbers.split('');
+var passwordOptions= [];
 
-console.log(numChar)
+
+//Generate prompts- if yes, add that array to the options array. Once all options have been selected, enter # of characters. Once all of this is done, for each character, choose one of the selected arrays randomly, then choose a random character from that array. Repeat for the number of characters selected. Then push the text onto the screen
+
+function generateRandomNum(len){
+
+  return Math.floor(Math.random() * len)
+
+ }
+
+
+function includeLowerCase(){
+  var choice = window.confirm("Do you want to include lowercase characters?");
+
+  if (choice){
+
+    passwordOptions.push(lowerCase);
+    console.log(passwordOptions);
+  }
+
+  }
+
+  function generateRandomNum(len){
+
+   return Math.floor(Math.random() * len)
+
+  }
+
+  function includeUpperCase(){
+    var choice = window.confirm("Do you want to include uppercase characters?");
+  
+    if (choice){
+  
+      passwordOptions.push(lowerCase);
+      console.log(passwordOptions);
+    }
+  
+    }
+  
+    function generateRandomNum(len){
+  
+     return Math.floor(Math.random() * len)
+  
+    }
 
 
 function generatePassword(){
+  
+  includeLowerCase()
+  var randomArray = generateRandomNum(passwordOptions.length)
+  var randomChar = generateRandomNum(passwordOptions[randomArray].length)
+  console.log(passwordOptions[randomArray][randomChar])
 
+  
 }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
